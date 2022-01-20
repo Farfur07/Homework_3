b1d38509b29ff336a8ab6de0b2ed053e8d5cb494
A.F <farfur@AFs-MacBook-Pro.local>
2022-01-20 17:28:42 +0200

    Initial commit

---
+++ Task_1(1).py
@@ -0,0 +1,52 @@
+flat_number = int(input('Enter flat number: '))
+door_1 = flat_number
+if door_1 <= 36:
+    print('In 1 door floor: ')
+if flat_number >= 37:
+    print('There is no your flat in this door floor')
+else:
+    if flat_number <= 4:
+        print('flat on 1 floor')
+    elif flat_number <= 8:
+        print('flat on 2 floor')
+    elif flat_number <= 12:
+        print('flat on 3 floor')
+    elif flat_number <= 16:
+        print('flat on 4 floor')
+    elif flat_number <= 20:
+        print('flat on 5 floor')
+    elif flat_number <= 24:
+        print('flat on 6 floor')
+    elif flat_number <= 28:
+        print('flat on 7 floor')
+    elif flat_number <= 32:
+        print('flat on 8 floor')
+    elif flat_number <= 36:
+        print('flat on 9 floor')
+    else:
+        print('There is no your flat in this door floor')
+
+door_2 = flat_number
+if 36 < door_2 >= 72:
+    print('In 2 door floor: ')
+if flat_number >= 73:
+    print('There is no your flat in this door floor')
+else:
+    if flat_number <= 40:
+        print('flat on 1 floor')
+    elif flat_number <= 44:
+        print('flat on 2 floor')
+    elif flat_number <= 48:
+        print('flat on 3 floor')
+    elif flat_number <= 52:
+        print('flat on 4 floor')
+    elif flat_number <= 56:
+        print('flat on 5 floor')
+    elif flat_number <= 60:
+        print('flat on 6 floor')
+    elif flat_number <= 64:
+        print('flat on 7 floor')
+    elif flat_number <= 68:
+        print('flat on 8 floor')
+    elif flat_number <= 72:
+        print('flat on 9 floor')
---
+++ Task_1.py
@@ -0,0 +1,63 @@
+flat_number = int(input('Enter flat number: '))
+
+if door_1 in range(0, 36):
+    else flat_number <= 4:
+        print('flat on 1 floor')
+        break
+    elif flat_number <= 8:
+        print('flat on 2 floor')
+        break
+    elif flat_number <= 12:
+        print('flat on 3 floor')
+        break
+    elif flat_number <= 16:
+        print('flat on 4 floor')
+        break
+    elif flat_number <= 20:
+        print('flat on 5 floor')
+        break
+    elif flat_number <= 24:
+        print('flat on 6 floor')
+        break
+    elif flat_number <= 28:
+        print('flat on 7 floor')
+        break
+    elif flat_number <= 32:
+        print('flat on 8 floor')
+        break
+    elif flat_number <= 36:
+        print('flat on 9 floor')
+        break
+    elif flat_number >= 37:
+        try: int(door_2)
+
+        for door_2 in range(37, 73):
+            if flat_number <= 40:
+                print('flat on 1 floor')
+                break
+            elif flat_number <= 44:
+                print('flat on 2 floor')
+                break
+            elif flat_number <= 48:
+                print('flat on 3 floor')
+                break
+            elif flat_number <= 52:
+                print('flat on 4 floor')
+                break
+            elif flat_number <= 56:
+                print('flat on 5 floor')
+                break
+            elif flat_number <= 60:
+                print('flat on 6 floor')
+                break
+            elif flat_number <= 64:
+                print('flat on 7 floor')
+                break
+            elif flat_number <= 68:
+                print('flat on 8 floor')
+                break
+            elif flat_number <= 72:
+                print('flat on 9 floor')
+                break
+
+
